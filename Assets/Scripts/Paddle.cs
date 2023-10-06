aa98d2cf9611773efcbd600481c8042283b3a3fe
mariskoch <maris@koch-24.de>
2023-10-06 21:21:04 +0200

    paddle adapts to growing and shrinking

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,17 +3,23 @@ using Vector3 = UnityEngine.Vector3;
 
 public class Paddle : MonoBehaviour
 {
-    public float speed = 0.2f;
+    public float speed;
     private Rigidbody2D rb;
     private Vector3 movement = Vector3.zero;
+    private SpriteRenderer sr;
+    private BoxCollider2D bc;
 
     private void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
+        sr = this.GetComponent<SpriteRenderer>();
+        bc = this.GetComponent<BoxCollider2D>();
     }
 
     private void Update()
     {
+        AdaptBoxColliderToSpriteSize();
+        
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             movement = new Vector3(1 * speed, 0, 0);
@@ -28,5 +34,14 @@ public class Paddle : MonoBehaviour
             movement = Vector3.zero;
         }
         rb.velocity = movement;
+    }
+
+    private void AdaptBoxColliderToSpriteSize()
+    {
+        if (!bc.size.x.Equals(sr.size.x))
+        {
+            Vector2 size = sr.size;
+            bc.size = new Vector2(size.x, size.y);
+        }
     }
 }
