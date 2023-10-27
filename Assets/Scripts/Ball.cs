d997f05997f92eee871ca7bacde33d665a17f6d0
Maris Koch <maris.koch@daimler.com>
2023-10-27 15:19:23 +0200

    new levels and ball does not get stuck horizontally

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,13 +1,35 @@
 using System;
 using UnityEngine;
+using Utils;
 
 public class Ball : MonoBehaviour
 {
-    public static event Action<Ball> OnBallDeath; 
+    [SerializeField] private float verticalSupportThreshold = 0.5f;
+    [SerializeField] private float supportImpulse = 0.5f;
+    private Rigidbody2D _rb;
     
+    public static event Action<Ball> OnBallDeath;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (GameManager.Instance.GameState != GameState.GameRunning) return;
+        var velocity = _rb.velocity;
+        if (Math.Abs(velocity.y) <= verticalSupportThreshold)
+        {
+            var isMovingUp = velocity.y > 0;
+            _rb.velocity = new Vector2(velocity.x, isMovingUp ? supportImpulse : -supportImpulse);
+        }
+    }
+
     public void Die()
     {
         OnBallDeath?.Invoke(this);
         Destroy(gameObject, 1);
     }
 }
+
