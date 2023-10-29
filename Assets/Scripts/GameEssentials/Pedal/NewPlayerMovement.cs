a25a6026d1584500ac0faeec980ac85874b17b8c
mariskoch <maris@koch-24.de>
2023-10-29 23:44:01 +0100

    started working with input system

---
+++ Assets/Scripts/GameEssentials/Pedal/NewPlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace GameEssentials.Pedal
+{
+    public class NewPlayerMovement : MonoBehaviour
+    {
+        private Movement _movement;
+
+        private void Awake()
+        {
+            _movement = new Movement();
+        }
+
+        private void OnEnable()
+        {
+            _movement.Player.Enable();
+            _movement.Player.Movement.performed += OnPlayerMovementPerformed;
+            _movement.Player.Movement.canceled += OnPlayerMovementCancelled;
+        }
+
+        private void OnDisable()
+        {
+            _movement.Player.Disable();
+            _movement.Player.Movement.performed -= OnPlayerMovementPerformed;
+            _movement.Player.Movement.canceled -= OnPlayerMovementCancelled;
+        }
+
+        private void OnPlayerMovementPerformed(InputAction.CallbackContext context)
+        {
+            var input = context.ReadValue<float>();
+            
+            // TODO: Work further with input from keys to change pedal movement
+            Debug.Log("Input: " + input);
+        }
+
+        private void OnPlayerMovementCancelled(InputAction.CallbackContext context)
+        {
+            
+        }
+    }
+}
