7d3e8d50c58b398b81f05fc93db1704f25b67bd4
MARIS KOCH <maris.koch@mercedes-benz.com>
2024-03-29 20:32:38 +0100

    button sounds are working

---
+++ Assets/Scripts/Menu/MenuButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Menu
+{
+    public class MenuButton : MonoBehaviour
+    {
+        [SerializeField] private AudioClip hoverSound;
+        [SerializeField] private AudioClip clickSound;
+        private AudioSource _as;
+
+        private void Awake()
+        {
+            _as = this.GetComponent<AudioSource>();
+        }
+
+        public void HandleHover()
+        {
+            if (hoverSound != null) _as.PlayOneShot(hoverSound);
+        }
+
+        public void HandleClick()
+        {
+            if (clickSound != null) _as.PlayOneShot(clickSound);
+        }
+    }
+}
