f1a6f7d69ac2c4f8dec9f6cd440c713ec7a1629f
MARIS KOCH <maris.koch@mercedes-benz.com>
2024-03-30 10:29:54 +0100

    menu: tutorial button available

---
+++ Assets/Scripts/Menu/Main/TutorialButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Menu.Main
+{
+    public class TutorialButton : MonoBehaviour
+    {
+        public void StartTutorial()
+        {
+            SceneManager.LoadScene("Tutorial1");
+        }
+    }
+}
